0b1c717053f91861563d0fa4da75903d3674d3b6
hirnidrin <info@itsu.ch>
2023-09-29 23:21:17 +0200

    fix(build): Try specific mirror for fedora-updates.repo, not mirrors metalink.

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 set -ouex pipefail
 
+# force use of single rpmfusion mirror
+sed -i.bak 's%^metalink=%#metalink=%' /etc/yum.repos.d/fedora-updates.repo
+sed -i 's%^#baseurl=http://download.example/pub/fedora/linux/updates/$releasever/Everything/$basearch/%baseurl=http://mirror.init7.net/fedora/fedora/linux/updates/38/Everything/x86_64/%' /etc/yum.repos.d/fedora-updates.repo
+
 RELEASE="$(rpm -E %fedora)"
 
 INCLUDED_PACKAGES=($(jq -r "[(.all.include | (select(.\"$PACKAGE_LIST\" != null).\"$PACKAGE_LIST\")[]), \
@@ -27,3 +31,6 @@ if [[ "${#EXCLUDED_PACKAGES[@]}" -gt 0 ]]; then
     rpm-ostree override remove \
         ${EXCLUDED_PACKAGES[@]}
 fi
+
+# reset forced use of single rpmfusion mirror
+rename -v .repo.bak .repo /etc/yum.repos.d/fedora-updates.repo.bak
